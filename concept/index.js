843176bcfa2e127b7303a62ba192397d6abf87a6
Tahmid Sajin <tahmid.saj@gmail.com>
2023-06-21 15:18:55 -0400

    Adding axios to gpt api

--- concept/index.js
+++ concept/index.js
@@ -1,20 +1,42 @@
 const { Configuration, OpenAIApi } = require("openai");
+const axios = require("axios");
+
 require("dotenv").config();
 
-const configuration = new Configuration({
-    apiKey: process.env.OPENAI_API_KEY
+const openai = axios.create({
+    baseURL: "https://api.openai.com/v1",
+    headers: {
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${process.env.OPENAI_API_KEY}`,
+    }
 });
-const openai = new OpenAIApi(configuration);
 
-async function runCompletion() {
-    const res = await openai.createCompletion({
-        model: "text-davinci-003",
-        prompt: "Can you please help me manage my time better?",
-        max_tokens: 4000,
+async function createChatCompletion(messages, options = {}) {
+    try {
+        const response = await openai.post("/chat/completions", {
+            model: options.model || "gpt-3.5-turbo",
+            messages,
+            ...options
+        });
+
+        return response.data.choices;
+    } catch (error) {
+        console.error("Failted to create chat completion: ", error);
+    }
+};
+
+async function main() {
+    const messages = [
+        { role: "user", content: "Can you please help me manage my time better?" },
+    ];
+
+    const options = {
         temperature: 1,
-    });
+        max_tokens: 4000
+    };
 
-    console.log(res.data.choices[0]);
+    const choices = await createChatCompletion(messages, options);
+    console.log(choices[0].message);
 }
 
-runCompletion();
+main();
