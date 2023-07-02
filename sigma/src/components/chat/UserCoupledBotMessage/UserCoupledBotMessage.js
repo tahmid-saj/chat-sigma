0fb1ddebaf860ebabc3f91a7a2a7b53717f9d7e1
Tahmid Sajin <tahmid.saj@gmail.com>
2023-07-02 02:30:38 -0400

    Updating user coupled bot message

--- sigma/src/components/chat/UserCoupledBotMessage/UserCoupledBotMessage.js
+++ sigma/src/components/chat/UserCoupledBotMessage/UserCoupledBotMessage.js
@@ -1,9 +1,13 @@
 import React, { Component } from "react";
 
+import UserMessage from "../UserMessage/UserMessage";
+import BotMessage from "../BotMessage/BotMessage";
+
 const UserCoupledBotMessage = () => {
   return (
     <div>
-      
+      <UserMessage/>
+      <BotMessage/>
     </div>
   );
 };
