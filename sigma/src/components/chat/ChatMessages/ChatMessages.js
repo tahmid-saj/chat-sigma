fbf2cab342f5164589d47fd0ecd6799a2e07df73
Tahmid Sajin <tahmid.saj@gmail.com>
2023-07-02 02:26:49 -0400

    Adding chat messages

---
+++ sigma/src/components/chat/ChatMessages/ChatMessages.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+import UserCoupledBotMessage from "../UserCoupledBotMessage/UserCoupledBotMessage";
+
+const ChatMessages = () => {
+  return (
+    <div>
+      <UserCoupledBotMessage/>
+    </div>
+  );
+};
+
+export default ChatMessages;
