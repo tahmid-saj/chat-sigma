99f3e9b93521e5f9b12cf3d2fb1dda5c11f61950
Tahmid Sajin <tahmid.saj@gmail.com>
2023-07-02 02:31:08 -0400

    Updating user message

---
+++ sigma/src/components/chat/UserMessage/UserMessage.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+const UserMessage = () => {
+  return (
+    <div>
+      <p>User's chat message</p>
+    </div>
+  );
+};
+
+export default UserMessage;
