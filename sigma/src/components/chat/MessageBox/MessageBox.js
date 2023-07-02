82d062b8c4e606eb8dbf5ac7ac64fbe5438d9abd
Tahmid Sajin <tahmid.saj@gmail.com>
2023-07-02 07:34:13 -0400

    Updating msg box

--- sigma/src/components/chat/MessageBox/MessageBox.js
+++ sigma/src/components/chat/MessageBox/MessageBox.js
@@ -3,13 +3,13 @@ import React, { Component } from "react";
 import "./MessageBox.css";
 import sendMessage from "./send-message.png";
 
-const MessageBox = () => {
+const MessageBox = ({ onUserMessageInputChange, onSendButtonSubmit }) => {
   return (
     <div className="center pa4 br3 shadow-5 messageBox">
-      <input className="f4 pa2 w-70 center messageBoxInput" type="text"/>
+      <input className="f4 pa2 w-70 center messageBoxInput" type="text" onChange={onUserMessageInputChange}/>
 
-      <button type="submit" className="btnSendMessage w-10 grow white">
-        <img className="btnSendMessage" alt="send-message" src={sendMessage} border="0"/>
+      <button type="submit" className="btnSendMessage w-10 grow white" onClick={onSendButtonSubmit}>
+        <img className="btnSendMessage" alt="send-message" src={sendMessage} width={45} height={45}/>
       </button>
     </div>
   );
