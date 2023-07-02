f00a3c11cf06cc68203cd67c045a4b6c979887a0
Tahmid Sajin <tahmid.saj@gmail.com>
2023-07-02 07:34:50 -0400

    Updating coupled bot msg

--- sigma/src/components/chat/UserCoupledBotMessage/UserCoupledBotMessage.js
+++ sigma/src/components/chat/UserCoupledBotMessage/UserCoupledBotMessage.js
@@ -1,15 +1,17 @@
 import React, { Component } from "react";
 
+import "./UserCoupledBotMessage.css";
+
 import UserMessage from "../UserMessage/UserMessage";
 import BotMessage from "../BotMessage/BotMessage";
 
-const UserCoupledBotMessage = () => {
+const UserCoupledBotMessage = ({ userBotMessageCouples }) => {
   return (
     <div>
-      <UserMessage/>
-      <BotMessage/>
+        {userBotMessageCouples}
     </div>
-  );
+    
+  )
 };
 
 export default UserCoupledBotMessage;
