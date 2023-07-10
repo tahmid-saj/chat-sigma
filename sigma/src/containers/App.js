4e2adcb0155c67f999f63441616000afddb1d3f7
Tahmid Sajin <tahmid.saj@gmail.com>
2023-07-09 22:56:49 -0400

    Code cleanup

--- sigma/src/containers/App.js
+++ sigma/src/containers/App.js
@@ -22,31 +22,142 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      messageInput: "",
+      userId: "1234567",
+      userMessageInput: "",
+      botMessageOutput: "",
       route: "chat",
       chatInitiated: false,
+      userInitiated: false,
       userBotMessageCouples: []
     }
   }
 
-  onUserMessageInputChange = (event) => {
-    this.setState({ messageInput: event.target.value });
+  // componentDidMount() {
+  //   fetch("http://localhost:3000/")
+  //   .then((response) => {
+  //     return response.json();
+  //   })
+  //   .then((data) => {
+  //     console.log(data);
+  //   });
+  // }
+
+  initiateChatSession = (userId, userMessageInput, botMessageOutput) => {
+    fetch("http://localhost:3000/initiateChat", {
+      method: "post",
+      headers: {"Content-Type": "application/json"},
+      body: JSON.stringify({
+        userId: userId,
+        userMessage: userMessageInput,
+        botMessage: botMessageOutput
+      })
+    })
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      if (data === "chat initiated") {
+        console.log("Chat initiated");
+      } else {
+        console.log("Chat could not be initiated");
+      }
+    })
   }
 
-  onSendButtonSubmit = () => {
-    console.log(this.state.messageInput);
+  sendUserBotMessage = (userId, userMessageInput, botMessageOutput) => {
+    fetch("http://localhost:3000/sendMessage", {
+      method: "put",
+      headers: {"Content-Type": "application/json"},
+      body: JSON.stringify({
+        userId: userId,
+        userMessage: userMessageInput,
+        botMessage: botMessageOutput
+      })
+    })
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      if (data === "message registered") {
+        console.log("Message registered");
+      } else {
+        console.log("Message could not be registered");
+      }
+    })
+  }
+
+  onUserMessageInputChange = (event) => {
+    this.setState({ userMessageInput: event.target.value });
+  }
+
+  onSendButtonSubmit = async () => {
     this.setState({ chatInitiated: true });
 
-    this.onNewUserCoupledBotMessage(this.state.messageInput);
-  }
+    // async function createChatCompletion(messages, options = {}) {
+    //   try {
+    //     const response = await openai.post("/chat/completions", {
+    //         model: options.model || "gpt-3.5-turbo",
+    //         messages,
+    //         ...options
+    //     });
+    
+    //     console.log(response.data.choices[0].message.content);
+    //     return response.data.choices[0].message.content;
+    //   } catch (error) {
+    //       console.error("Failted to create chat completion: ", error);
+    //   }
+    // };
 
-  onNewUserCoupledBotMessage(userMessageInput) {
+    // async function initiateGPTAPICall(userMessageInput) {
+    //   const messages = [
+    //       { role: "user", content: userMessageInput },
+    //   ];
+  
+    //   const options = {
+    //       temperature: 1,
+    //       max_tokens: 4000
+    //   };
+  
+    //   return await createChatCompletion(messages, options);
+    // };
+
+    // initiateGPTAPICall(this.state.userMessageInput);
+
+    // await this.onNewUserCoupledBotMessage(this.state.userMessageInput, await initiateGPTAPICall(this.state.userMessageInput));
+    
+    await this.onNewUserCoupledBotMessage(this.state.userMessageInput, 
+      await fetch("http://localhost:3000/getMessageQuery", {
+        method: "post",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify({
+          userMessage: this.state.userMessageInput
+        })}
+      )
+      .then((response) => {
+        return response.json();
+      })
+    );
+  }
+  
+
+  async onNewUserCoupledBotMessage(userMessageInput, botMessageOutput) {
     this.state.userBotMessageCouples.push(
       <div className='userCoupledBotMessage'>
-        <UserMessage userMessageInput={userMessageInput}/>
-        <BotMessage/>
+        <UserMessage key={userMessageInput} userMessageInput={userMessageInput}/>
+        <BotMessage key={botMessageOutput} botMessageOutput={botMessageOutput}/>
       </div>
     )
+
+    // send user and bot message
+    if (this.state.userInitiated) {
+      this.sendUserBotMessage(this.state.userId, userMessageInput, botMessageOutput);
+    }
+
+    // initiate user's chat session
+    if (!this.state.userInitiated) {
+      this.initiateChatSession(this.state.userId, userMessageInput, botMessageOutput);
+      this.setState({ userInitiated: true });
+    }
   }
 
   onRouteChange = (route) => {
@@ -63,6 +174,11 @@ class App extends Component {
           this.state.route === "chat" ?
             <div>
               {/* Chat page below */}
+              <MessageBox
+                onUserMessageInputChange={this.onUserMessageInputChange} 
+                onSendButtonSubmit={this.onSendButtonSubmit}
+              />
+
               <ChatMessagesScroll>
                 {
                   this.state.chatInitiated === false ?
@@ -73,10 +189,6 @@ class App extends Component {
                     />
                 }
               </ChatMessagesScroll>
-              <MessageBox
-                onUserMessageInputChange={this.onUserMessageInputChange} 
-                onSendButtonSubmit={this.onSendButtonSubmit}
-              />
             </div>
             : (
               this.state.route === "export" ?
