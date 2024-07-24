9427679470c7040642e5de0eaf61da0eecbd8909
tahmid-saj <tahmid.saj@gmail.com>
2024-07-23 20:23:16 -0400

    messages margin bottom

--- src/components/chat/chat-messages/chat-messages.styles.jsx
+++ src/components/chat/chat-messages/chat-messages.styles.jsx
@@ -2,5 +2,5 @@ import styled from "styled-components"
 import { COLOR_CODES } from "../../../utils/constants/shared.constants"
 
 export const ChatMessagesContainer = styled.div`
-
+  margin: 0% 0% 30% 0%;
 `
